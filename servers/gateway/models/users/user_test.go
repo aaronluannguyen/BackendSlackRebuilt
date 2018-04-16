10a65cdd2d91b6d7abea82f68068dc4fdacf9a59
aaronluannguyen <aaronluannguyen@gmail.com>
2018-04-15 19:37:25 -0700

    FInished tests for authenticate

--- servers/gateway/models/users/user_test.go
+++ servers/gateway/models/users/user_test.go
@@ -125,11 +125,100 @@ func TestToUser(t *testing.T) {
 }
 
 func TestFullName(t *testing.T) {
+	cases := []struct {
+		name string
+		u User
+		expectedFullName string
+	}{
+		{
+			"Normal First & Last Name",
+			User{
+				FirstName: "First",
+				LastName: "Last",
+			},
+			"First Last",
+		},
+		{
+			"No First Name",
+			User{
+				LastName: "Last",
+			},
+			"Last",
+		},
+		{
+			"No Last Name",
+			User{
+				FirstName: "First",
+			},
+			"First",
+		},
+		{
+			"No First or Last Name",
+			User{},
+			"",
+		},
+	}
 
+	for _, c := range cases {
+		fullName := c.u.FullName()
+		if fullName != c.expectedFullName {
+			t.Errorf("case: %s: expected full name does not match with produced full name", c.name)
+		}
+	}
 }
 
 func TestAuthenticate(t *testing.T) {
+	user := User{}
+	user.SetPassword("123456Abc")
+	userEmptyString := User{}
+	userEmptyString.SetPassword("")
 
+	cases := []struct {
+		name string
+		user User
+		userPassword []byte
+		testPassword string
+		err bool
+	}{
+		{
+			"Correct Password",
+			user,
+			user.PassHash,
+			"123456Abc",
+			false,
+		},
+		{
+			"Incorrect Password",
+			user,
+			user.PassHash,
+			"123456Abc123",
+			true,
+		},
+		{
+			"Empty String Password Correct",
+			userEmptyString,
+			userEmptyString.PassHash,
+			"",
+			false,
+		},
+		{
+			"Empty String Password Incorrect",
+			userEmptyString,
+			userEmptyString.PassHash,
+			"not empty",
+			true,
+		},
+	}
+
+	for _, c := range cases {
+		err := c.user.Authenticate(c.testPassword)
+		if err != nil && !c.err {
+			t.Errorf("case: %s: unexpected error", c.name)
+		}
+		if err == nil && c.err {
+			t.Errorf("case %s: expected error but didn't get one", c.name)
+		}
+	}
 }
 
 func TestApplyUpdates(t *testing.T) {
