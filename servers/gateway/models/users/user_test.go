6efb9e3deccb41d49a5e5e6382ff042fe5cb2bad
aaronluannguyen <aaronluannguyen@gmail.com>
2018-04-15 18:17:50 -0700

    Set up user tests in models

--- servers/gateway/models/users/user_test.go
+++ servers/gateway/models/users/user_test.go
@@ -1,4 +1,26 @@
 package users
 
+import "testing"
+
 //TODO: add tests for the various functions in user.go, as described in the assignment.
 //use `go test -cover` to ensure that you are covering all or nearly all of your code paths.
+
+func TestValidate(t *testing.T) {
+
+}
+
+func TestToUser(t *testing.T) {
+
+}
+
+func TestFullName(t *testing.T) {
+
+}
+
+func TestAuthenticate(t *testing.T) {
+
+}
+
+func TestApplyUpdates(t *testing.T) {
+
+}
