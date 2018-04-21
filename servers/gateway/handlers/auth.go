78764ed0fe29a683ae51a231ac4ee68e1771ef68
aaronluannguyen <aaronluannguyen@gmail.com>
2018-04-20 17:48:07 -0700

    Finished conext struct and initialized auth go file with all needed functions

--- servers/gateway/handlers/auth.go
+++ servers/gateway/handlers/auth.go
@@ -4,3 +4,18 @@ package handlers
 //assignment description. Remember to use your handler context
 //struct as the receiver on these functions so that you have
 //access to things like the session store and user store.
+func (ctx *Context) UsersHandler() {
+
+}
+
+func (ctx *Context) SpecificUserHandler() {
+
+}
+
+func (ctx *Context) SessionsHandlerU() {
+
+}
+
+func (ctx *Context) SpecificSessionHandler() {
+
+}
