aeeaaa80619f8fd97405dd71a8f4484bcf4067ce
aaronluannguyen <aaronluannguyen@gmail.com>
2018-04-21 01:55:23 -0700

    Finished CORS handler

--- servers/gateway/handlers/cors.go
+++ servers/gateway/handlers/cors.go
@@ -1,5 +1,7 @@
 package handlers
 
+import "net/http"
+
 /* TODO: implement a CORS middleware handler, as described
 in https://drstearns.github.io/tutorials/cors/ that responds
 with the following headers to all requests:
@@ -10,3 +12,16 @@ with the following headers to all requests:
   Access-Control-Expose-Headers: Authorization
   Access-Control-Max-Age: 600
 */
+
+const methodsCORS = "Access-Control-Allow-Methods"
+const allowHeadersCORS = "Access-Control-Allow-Headers"
+const exposeHeadersCORS = "Access-Control-Expose-Headers"
+const maxAgeCORS = "Access-Control-Max-Age"
+
+func corsHandler(w http.ResponseWriter) {
+	w.Header().Add(headerCORS, "*")
+	w.Header().Add(methodsCORS, "GET, PUT, POST, PATCH, DELETE")
+	w.Header().Add(allowHeadersCORS, "Content-Type, Authorization")
+	w.Header().Add(exposeHeadersCORS, "Authorization")
+	w.Header().Add(maxAgeCORS, "600")
+}
