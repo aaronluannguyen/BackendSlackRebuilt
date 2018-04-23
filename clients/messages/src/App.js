3a739466492e353536126dde2b53b911aa2a5d86
aaronluannguyen <aaronluannguyen@gmail.com>
2018-04-23 14:05:22 -0700

    Added router info for sign up details

--- clients/messages/src/App.js
+++ clients/messages/src/App.js
@@ -2,6 +2,10 @@ import React, { Component } from 'react';
 import {HashRouter as Router, Switch, Redirect, Route} from "react-router-dom";
 import {ROUTES} from "./constants";
 import './App.css';
+import SignUpDetailsView from "./Views/SignUpDetails";
+import SignUpView from "./Views/SignUp";
+import SignInView from "./Views/SignIn";
+import MainView from "./Views/Main";
 
 class App extends Component {
   render() {
@@ -10,6 +14,7 @@ class App extends Component {
           <Switch>
               <Route exact path={ROUTES.signIn} component={SignInView} />
               <Route path={ROUTES.signUp} component={SignUpView}/>
+              <Route path={ROUTES.signUpDetails} component={SignUpDetailsView}/>
               <Route path={ROUTES.main} component={MainView}/>
               <Redirect to={ROUTES.signIn}/>
           </Switch>
--- clients/messages/src/constants.js
+++ clients/messages/src/constants.js
@@ -1,6 +1,7 @@
 export const ROUTES = {
     signIn: "/",
     signUp: "/signup",
+    signUpDetails: "/signupDetails",
     main: "/channels/:channelName",
     generalChannel: "/channels/general"
 }
