2a85fe2072cb59fcc1d86a9b9051d42124f2cff6
aaronluannguyen <aaronluannguyen@gmail.com>
2018-05-02 02:39:34 -0700

    Made changes to db scripts

--- servers/db/dockerBuild.sh
+++ servers/db/dockerBuild.sh
@@ -4,8 +4,8 @@ export MYSQL_DATABASE=users
 
 docker rm -f usersdb
 docker run -d \
--p 127.0.0.1:3306:3306 \
+--network aaronchatnet \
 --name usersdb \
 -e MYSQL_ROOT_PASSWORD=$MYSQL_ROOT_PASSWORD \
 -e MYSQL_DATABASE=$MYSQL_DATABASE \
-aaronluannguyen/users
+aaronluannguyen/usersdb
