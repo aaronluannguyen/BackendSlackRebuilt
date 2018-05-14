dfbca424df6a2536bf0312c19ebd21e1cfb957f6
aaronluannguyen <aaronluannguyen@gmail.com>
2018-05-14 01:44:45 -0700

    Added new tabels to db shema for channel and messages

--- servers/db/schema.sql
+++ servers/db/schema.sql
@@ -9,3 +9,27 @@ create table if not exists users (
   unique(email),
   unique(username)
 );
+
+create table if not exists channels (
+  id int primary key auto_increment not null,
+  name varchar(255) not null,
+  description varchar(1024) not null,
+  private boolean not null,
+  createdAt timestamp not null,
+  creatorUserID int null,
+  editedAt timestamp not null
+);
+
+create table if not exists members (
+  memberID int not null,
+  channelID int not null
+);
+
+create table if not exists messages (
+  id int primary key auto_increment not null,
+  channelID int not null,
+  body varchar(2083) not null,
+  createdAt timestamp not null,
+  creatorUserID int not null,
+  editedAt timestamp not null
+);
